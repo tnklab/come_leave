5c332435f208172654bff0222a48f2cff380548c
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2018-05-08 15:47:22 +0900

    変更

--- felica/kazu/test.py
+++ felica/kazu/test.py
@@ -1,23 +1,24 @@
+# read_idm.py
 import binascii
 import nfc
-import time
-from threading import Thread,Timer
-
 
 class MyCardReader(object):
-    def on_connect(self,tag):
+    def on_connect(self, tag):
         print("touched")
         self.idm = binascii.hexlify(tag.idm)
         return True
+
     def read_id(self):
         clf = nfc.ContactlessFrontend('usb')
         try:
             clf.connect(rdwr={'on-connect': self.on_connect})
         finally:
             clf.close()
+
 if __name__ == '__main__':
     cr = MyCardReader()
-    print("Touch card:")
-    cr.read_id()
-    print("released")
-    print(cr.idm)
+    while True:
+        print ("touch card:")
+        cr.read_id()
+        print("released")
+        print(cr.idm)
