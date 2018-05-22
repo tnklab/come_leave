703a77ffa080d8ad5796f6455168e89d6bcbbd81
kazu-mottan <kaitoxtennis1997mail@gmail.com>
2018-05-22 13:56:05 +0900

    モデルを修正

--- app/lab/lab/kazu/models.py
+++ app/lab/lab/kazu/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
+
+class Kazu(models.Model):
+    content = models.CharField(max_length = 140,verbose_name = '本文')
+    posted_date = models.DataTimeField(auto_now_add = True)
+
+
+
 # Create your models here.
